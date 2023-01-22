1796c1fa65610aec9fa0096e2e9330de6ef151e2
Haruki Hanagata <haruzo28@icloud.com>
2023-01-23 07:13:19 +0900

    Initial Commit

---
+++ SpeachAlarmSample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SpeachAlarmSample
+//
+//  Created by 花形春輝 on 2023/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpeachAlarmSample/SpeachAlarmSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeachAlarmSampleApp.swift
+//  SpeachAlarmSample
+//
+//  Created by 花形春輝 on 2023/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SpeachAlarmSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
